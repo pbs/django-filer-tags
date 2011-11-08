1e82e6ad12c854f5917a4a697ff48a06316ba96d
Sever Băneşiu <banesiu.sever@gmail.com>
2011-11-08 14:17:55 +0200

    Added local regex cache and fixed regex.

--- filertags/views.py
+++ filertags/views.py
@@ -10,12 +10,15 @@ def css_preprocessor(request, path):
     f = filerthumbnail(path)
     if not f and path.endswith('.css'):
         raise Http404('No such file.')
+    local_cache = {}
 
     def change_urls(match):
         relative_path = match.groups()[0]
-        path = os.path.join(dirname, relative_path)
-        return "url('%s')" % filerfile(path)
+        path = os.path.normpath(os.path.join(dirname, relative_path))
+        if not path in local_cache:
+            local_cache[path] = "url('%s')" % filerfile(path)
+        return local_cache[path]
 
     return HttpResponse(re.sub(
-        r"url\(['\"]([^']+)['\"]\)", change_urls, f.read()
+        r"url\(['\"]([^'\"]+)['\"]\)", change_urls, f.read()
     ))
