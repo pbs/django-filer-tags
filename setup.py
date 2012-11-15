e491d790e5c80ce86cc2432c2f8c733095979535
Alex Cucu <alexandruioan.cucu@gmail.com>
2012-11-15 14:35:23 +0200

    Bumped version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ dependency_links = [
 
 setup(
     name='django-filer-tags',
-    version='0.1',
+    version='0.2',
     description='Extra template filters and tags for filer',
     long_description = open(README_PATH, 'r').read(),
     author='Sever Banesiu',
