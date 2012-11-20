c7cd2af3ffb86ae7586684fd2c32c8de60405bbd
Bogdan Hodorog <bogdan.hodorog@threepillarglobal.com>
2012-11-20 10:31:55 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ dependency_links = [
 
 setup(
     name='django-filer-tags',
-    version='0.3',
+    version='0.4',
     description='Extra template filters and tags for filer',
     long_description = open(README_PATH, 'r').read(),
     author='Sever Banesiu',
