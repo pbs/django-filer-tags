b6ff0cdcac2f9c78d832880b30ab5fcfe9ba9751
Zoltan Szeredi <zoltan.szeredi@3pillarglobal.com>
2015-09-23 16:30:02 +0300

    Bump version to 1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ dependency_links = [
 
 setup(
     name='django-filer-tags',
-    version='1.0.0',
+    version='1.1.0',
     description='Extra template filters and tags for filer',
     long_description = open(README_PATH, 'r').read(),
     author='Sever Banesiu',
